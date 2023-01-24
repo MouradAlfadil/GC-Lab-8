e64f230c63ba17d8dd199939ddb9aa45778f9849
MouradAlfadil <119465416+MouradAlfadil@users.noreply.github.com>
2023-01-24 08:59:04 -0500

    Add files via upload

---
+++ GC Lab 9/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lab_9
+{
+    internal class Circle
+    {
+
+        private double Radius;
+
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return Math.Round(CalculateCircumference(), 2).ToString();
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public string CalculateFormattedArea()
+        {
+            return Math.Round(CalculateArea(), 2).ToString();
+        }
+        private string FormatNumber(double x)
+        {
+
+            return x.ToString();
+
+        }
+
+    }
+}
---
+++ GC Lab 9/Program.cs
@@ -0,0 +1,69 @@
+﻿using GC_Lab_9;
+
+bool isrunning = true;
+List<Circle> circles = new List<Circle>();
+double choice = 0;
+
+while (isrunning)
+{
+    //greet
+    Console.WriteLine("Hello, please enter a radius");
+    //collect response
+    while (true)
+    {
+
+
+        try
+        {
+            choice = double.Parse(Console.ReadLine());
+            if (choice < 0)
+            {
+                Console.WriteLine("Number cannot be negative, please try again");
+                continue;
+            }
+            break;
+
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Incorrect input, please try again");
+            continue;
+        }
+    }
+
+    //construct new circle
+    Circle circ = new Circle(choice);
+    circles.Add(circ);
+    //post results
+    Console.WriteLine($"Circumference: {circ.CalculateFormattedCircumference()}");
+    Console.WriteLine($"Area: {circ.CalculateFormattedArea()}");
+
+    //ask if they want to continue
+    Console.WriteLine("Would you like to continue? (Enter y to continue, any other key to exit)");
+    if (Console.ReadLine().ToLower().Trim() == "y")
+    {
+        continue;
+    }
+    else
+    {
+        isrunning = false;
+        continue;
+    }
+
+
+}
+
+//cw goodbye and the total number of circles they made
+Console.WriteLine($"Goodbye. You have created {circles.Count} circles.");
+Console.Read();
+
+
+
+
+
+
+
+
+
+
+
